6d8542c10f146974c2c6ad1c7430b3595afa2e93
kdayun <rlaekdbs0329@naver.com>
2026-02-08 17:56:37 +0900

    first commit

---
+++ src/main/java/com/example/miniproj/MiniprojBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.miniproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniprojBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniprojBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/miniproj/MiniprojBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.miniproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniprojBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
