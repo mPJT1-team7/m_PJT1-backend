9ea949f296944b12b6ce7d313708b14f378ca92a
이승현 <npnp123npnp123@gmail.com>
2026-02-12 16:31:49 +0900

    Feat: PATCH 허용

--- src/main/java/com/example/miniproj/common/config/SecurityConfig.java
+++ src/main/java/com/example/miniproj/common/config/SecurityConfig.java
@@ -59,7 +59,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         
         configuration.setAllowedOriginPatterns(List.of("http://localhost:3000", "http://localhost:5173"));
-        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
 
